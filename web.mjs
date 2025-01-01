ecc1f66e8ffc579e1965490e1ab78e811bbade98
nin-jin <nin-jin@users.noreply.github.com>
2025-01-01 07:21:11 +0000

    Deploying to gh-pages from @ hyoo-ru/square.hyoo.ru@2451731600f66aaa55a3c3d8ba208f0c7b845e06 🚀

--- web.mjs
+++ web.mjs
@@ -3033,7 +3033,6 @@ var $;
             maxHeight: per(100),
             boxSizing: 'border-box',
             color: $mol_theme.text,
-            backdropFilter: blur(`3px`),
             ':focus': {
                 outline: 'none',
             },
